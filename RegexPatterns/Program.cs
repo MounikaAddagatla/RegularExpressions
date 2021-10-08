83ad488cc74e8d5c020532d9afa1f637d7d1d206
MounikaAddagatla <89588852+MounikaAddagatla@users.noreply.github.com>
2021-10-08 12:31:40 +0530

    Added: A valid pincode for postal

---
+++ RegexPatterns/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RegexPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Printing the Regular Expression Patterns");
+            RegexExmp pinCode = new RegexExmp();
+
+            // valid pincode //
+            pinCode.IsValidPinCode("400086");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegexPatterns/RegexExmp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexPatterns
+{
+    class RegexExmp
+    {
+        // CHECKING FOR VALIDATING INDIA  PINCODE WITHOUT CHARECTERS //
+        const string POSTAL_PIN_CHECK = "^[0-9]{6}$";
+
+        // Function to validate the pin code of India.
+       public void IsValidPinCode(string pinCode)
+       {
+
+        // Return true if the pin code
+        // matched the ReGex
+               if (Regex.IsMatch(pinCode,POSTAL_PIN_CHECK))
+               {
+                Console.WriteLine("Pincode is valid");
+                return;
+               }
+          Console.WriteLine("Not Valid Pincode");
+       }
+
+    }
+}
+
